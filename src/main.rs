aaac112143965026bb73f3dd0bdf9c6dd143d99d
Tomochika Hara <git@thara.dev>
2021-04-04 22:13:07 +0900

    Clean up

--- src/main.rs
+++ src/main.rs
@@ -299,9 +299,10 @@ struct State {
     index_buffer: wgpu::Buffer,
     num_vertices: u32,
 
+    camera: Camera,
     camera_controller: CameraController,
     uniforms: Uniforms,
-    uniform_staging: UniformStaging,
+    // uniform_staging: UniformStaging,
     uniform_buffer: wgpu::Buffer,
     uniform_bind_group: wgpu::BindGroup,
 
@@ -317,36 +318,6 @@ struct State {
 
     depth_texture: texture::Texture,
 }
-
-fn quat_mul(q: cgmath::Quaternion<f32>, r: cgmath::Quaternion<f32>) -> cgmath::Quaternion<f32> {
-    // This block uses quaternions of the form of
-
-    // q=q0+iq1+jq2+kq3
-
-    // and
-
-    // r=r0+ir1+jr2+kr3.
-
-    // The quaternion product has the form of
-
-    // t=q×r=t0+it1+jt2+kt3,
-
-    // where
-
-    // t0=(r0 q0 − r1 q1 − r2 q2 − r3 q3)
-    // t1=(r0 q1 + r1 q0 − r2 q3 + r3 q2)
-    // t2=(r0 q2 + r1 q3 + r2 q0 − r3 q1)
-    // t3=(r0 q3 − r1 q2 + r2 q1 + r3 q0
-
-    let w = r.s * q.s - r.v.x * q.v.x - r.v.y * q.v.y - r.v.z * q.v.z;
-    let xi = r.s * q.v.x + r.v.x * q.s - r.v.y * q.v.z + r.v.z * q.v.y;
-    let yj = r.s * q.v.y + r.v.x * q.v.z + r.v.y * q.s - r.v.z * q.v.x;
-    let zk = r.s * q.v.z - r.v.x * q.v.y + r.v.y * q.v.x + r.v.z * q.s;
-
-    cgmath::Quaternion::new(w, xi, yj, zk)
-}
-
-const ROTATION_SPEED: f32 = 2.0 * std::f32::consts::PI / 60.0;
 
 impl State {
     async fn new(window: &Window) -> Self {
@@ -440,8 +411,9 @@ impl State {
         let camera_controller = CameraController::new(0.2);
 
         let mut uniforms = Uniforms::new();
-        let uniform_staging = UniformStaging::new(camera);
-        uniform_staging.update_uniforms(&mut uniforms);
+        uniforms.update_view_proj(&camera);
+        // let uniform_staging = UniformStaging::new(camera);
+        // uniform_staging.update_uniforms(&mut uniforms);
 
         let uniform_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
             label: Some("Uniform Buffer"),
@@ -527,7 +499,7 @@ impl State {
         let instance_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
             label: Some("Instance Buffer"),
             contents: bytemuck::cast_slice(&instance_data),
-            usage: wgpu::BufferUsage::VERTEX | wgpu::BufferUsage::COPY_DST,
+            usage: wgpu::BufferUsage::VERTEX,
         });
 
         Self {
@@ -541,9 +513,9 @@ impl State {
             vertex_buffer,
             index_buffer,
             num_vertices,
+            camera,
             camera_controller,
             uniforms,
-            uniform_staging,
             uniform_buffer,
             uniform_bind_group,
             instances,
@@ -563,8 +535,6 @@ impl State {
 
         self.depth_texture =
             texture::Texture::create_depth_texture(&self.device, &self.sc_desc, "depth_texture");
-
-        self.uniform_staging.camera.aspect = self.sc_desc.width as f32 / self.sc_desc.height as f32;
     }
 
     fn input(&mut self, event: &WindowEvent) -> bool {
@@ -572,32 +542,12 @@ impl State {
     }
 
     fn update(&mut self) {
-        use cgmath::Rotation3;
-
-        self.camera_controller
-            .update_camera(&mut self.uniform_staging.camera);
-        self.uniform_staging.model_rotation += cgmath::Deg(2.0);
-        self.uniform_staging.update_uniforms(&mut self.uniforms);
+        self.camera_controller.update_camera(&mut self.camera);
+        self.uniforms.update_view_proj(&self.camera);
         self.queue.write_buffer(
             &self.uniform_buffer,
             0,
             bytemuck::cast_slice(&[self.uniforms]),
-        );
-
-        for instance in &mut self.instances {
-            let amount = cgmath::Quaternion::from_angle_y(cgmath::Rad(ROTATION_SPEED));
-            let current = instance.rotation;
-            instance.rotation = quat_mul(amount, current);
-        }
-        let instance_data = self
-            .instances
-            .iter()
-            .map(Instance::to_raw)
-            .collect::<Vec<_>>();
-        self.queue.write_buffer(
-            &self.instance_buffer,
-            0,
-            bytemuck::cast_slice(&instance_data),
         );
     }
 
